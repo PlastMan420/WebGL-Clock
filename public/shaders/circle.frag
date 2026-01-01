a0c4fd820a4d31f404138249d73aa53bce274094
PlastMAn420 <mostafa.alaa78@outlook.com>
2026-01-01 07:21:24 +0200

    Init

---
+++ public/shaders/circle.frag
@@ -0,0 +1,80 @@
+#version 300 es
+#define M_PI 3.1415926535897932384626433832795
+
+precision highp float;
+
+out vec4 fragColor;
+
+uniform vec2 u_resolution;
+uniform vec3 u_timeInput;// (hour, minute, second)
+
+float strokeCircle(float radius,float thickness,vec2 uv){
+  vec2 center=vec2(.5,.5);
+  float dist=distance(uv,center);
+  return step(radius-thickness,dist)-step(radius,dist);
+}
+
+float lineFromCenter(vec2 p1,vec2 p2,float thickness,vec2 uv){
+  vec2 pa=uv-p1;// uv - (.5, .5) = center (.5, .5)
+  vec2 ba=p2-p1;// (.5, .82) - (.5, .5) = (0, .32)
+  
+  // Projection of pa onto ba.
+  float h=clamp(dot(pa,ba)/dot(ba,ba),0.,1.);// = 0.16 / 1 = 0.16
+  float dist=length(pa-ba*h);// (.5, .5) - (0, 0.0512) = (0.5, 0.4488) = 0.671
+  return step(dist,thickness);
+}
+
+vec3 getAngleFromTimeInput(vec3 timeInput){
+  float hourAngle=mod(timeInput.x,12.)*30.+(timeInput.y/60.)*30.;// Each hour is 30 degrees + minute contribution
+  float minuteAngle=timeInput.y*6.+(timeInput.z/60.)*6.;// Each minute is 6 degrees + second contribution
+  float secondAngle=timeInput.z*6.;// Each second is 6 degrees
+  
+  return vec3(hourAngle,minuteAngle,secondAngle);
+}
+
+float angleToRadian(float angleDeg){
+  return angleDeg*M_PI/180.;
+}
+
+vec3 drawArrow(float angleDeg,float arrowLength,vec3 color,float thickness,vec2 uv){
+  vec2 center=vec2(.5,.5);
+  float angleDegToAngleRad=angleToRadian(angleDeg);
+  vec2 lineEnd=center+vec2(sin(angleDegToAngleRad),cos(angleDegToAngleRad))*arrowLength;
+  float lineShape=lineFromCenter(center,lineEnd,thickness,uv);
+  return color*lineShape;
+}
+
+void main(){
+  vec2 uv=gl_FragCoord.xy/u_resolution;// Normalized coordinates (0 to 1). dependent on resolution.
+  vec2 center=vec2(.5,.5);
+  
+  float radius=.4;
+  
+  float hoursArrowLength=radius*.6;
+  float minutesArrowLength=radius*.8;
+  float secondsArrowLength=radius*.9;
+  
+  vec3 hoursArrowColors=vec3(1.,0.,0.);
+  vec3 minutesArrowColors=vec3(0.,1.,0.);
+  vec3 secondsArrowColors=vec3(0.,0.,1.);
+  
+  // Clock Circle
+  float circle=strokeCircle(radius,.05,uv);
+  
+  // Clock Arrows
+  vec3 angles=getAngleFromTimeInput(u_timeInput);
+  vec3 hoursArrow=drawArrow(angles.x,hoursArrowLength,hoursArrowColors,.02,uv);
+  vec3 minutesArrow=drawArrow(angles.y,minutesArrowLength,minutesArrowColors,.015,uv);
+  vec3 secondsArrow=drawArrow(angles.z,secondsArrowLength,secondsArrowColors,.01,uv);
+  
+  vec3 color=vec3(1.,1.,1.)*circle;
+  color+=hoursArrow;
+  color+=minutesArrow;
+  color+=secondsArrow;
+  
+  vec4 outPutResult=vec4(color,1.);
+  
+  outPutResult=vec4(color,length(color)==0.?0.:1.);
+  
+  fragColor=outPutResult;
+}
---
+++ src/main.ts
@@ -0,0 +1,119 @@
+import "./style.css";
+
+const canvas = document.getElementById("app") as HTMLCanvasElement;
+const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
+
+const fragmentShaderSource = await fetch("/shaders/circle.frag").then((res) =>
+  res.text()
+);
+const vertexShaderSource = await fetch("/shaders/vertex.glsl").then((res) =>
+  res.text()
+);
+
+function createShader(
+  gl: WebGL2RenderingContext,
+  type: number,
+  source: string
+) {
+  const shader = gl.createShader(type);
+  if (!shader) {
+    console.error("Unable to create shader");
+    return null;
+  }
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+function createProgram(
+  gl: WebGL2RenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader
+) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return null;
+  }
+
+  return program;
+}
+
+// Create shaders and program
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(
+  gl,
+  gl.FRAGMENT_SHADER,
+  fragmentShaderSource
+);
+
+if (!vertexShader || !fragmentShader) {
+  throw new Error("Shader creation failed");
+}
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+
+if (!program) {
+  throw new Error("Program creation failed");
+}
+
+// Set up a full-screen quad
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const resolutionUniformLocation = gl.getUniformLocation(
+  program,
+  "u_resolution"
+);
+const timeUniformLocation = gl.getUniformLocation(program, "u_timeInput");
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+const positions = [-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1];
+
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+const vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+gl.enableVertexAttribArray(positionAttributeLocation);
+gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+// Render
+gl.viewport(0, 0, canvas.width, canvas.height);
+gl.clearColor(0, 0, 0, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.useProgram(program);
+gl.bindVertexArray(vao);
+gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+
+//gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+function animate() {
+  const now = new Date();
+  const hours = now.getHours() % 12;
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  //gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.uniform3f(timeUniformLocation, hours, minutes, seconds);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+  requestAnimationFrame(animate);
+}
+
+setInterval(() => {
+  animate();
+}, 1000);
